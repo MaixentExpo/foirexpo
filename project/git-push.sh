c42821c466cadd9384d97820f51f8743c5876d20
pbillerot <philippe.billerot@gmail.com>
2021-01-02 12:37:44 +0100

    ssh ovh

--- project/git-push.sh
+++ project/git-push.sh
@@ -2,16 +2,16 @@
 
 hugo -d /volshare/foirep --cleanDestinationDir
 
-# git init
-# git add .
-# git commit -m "version initiale"
-# git init --bare /volshare/src/foirexpo.git
-# git remote add volshare /volshare/src/foirexpo.git
-# git push volshare master
-# git branch --set-upstream-to=volshare/master master
+# Création d'un repository sur github
+# https://github.com/MaixentExpo/foirexpo
+# Déclarer SSH and GPG keys sur Personnal settings de pbillerot
+# Inviter pbillerot en tant que mamager du projet
+# En local
+# sudo git clone https://github.com/MaixentExpo/foirexpo.git
+# git remote set-url origin git@github.com:MaixentExpo/foirexpo.git
 
 git status
 git add .
 git commit -m "ok boumer"
 git pull
-git push volshare master
+git push origin master
